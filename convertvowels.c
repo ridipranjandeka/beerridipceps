0ae318e4b58603682f0df87b5665f4dd81844b22
ridip <ridipranjandeka35@gmail.com>
2022-10-28 10:07:16 -0700

    convertvowels.c

---
+++ convertvowels.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    
+    char str[100];  
+    int i, vowels = 0;
+    
+    
+    printf("Enter the string: ");
+    scanf("%[^\n]s",&str);
+    
+    
+    for(i = 0; str[i]; i++)  
+    {
+        //Counting the vowels.
+        if(str[i]=='a'|| str[i]=='e'||str[i]=='i'||
+           str[i]=='o'|| str[i]=='u'||str[i]=='A'||
+           str[i]=='E'||str[i]=='I'||str[i]=='O' ||str[i]=='U')
+        {
+            vowels++;
+        }
+    }
+ 	
+    //Printing the count of vowels.
+    printf("Total number of vowels: = %d\n",vowels);
+    
+    return 0;
+}
